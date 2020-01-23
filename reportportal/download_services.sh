18fdcf38e68dab6a50fe6d3ae6ff002eae37d495
ihar-kahadouski <igor.kagadowski@gmail.com>
2020-01-23 13:28:40 +0300

    Separate versions for services

--- reportportal/download_services.sh
+++ reportportal/download_services.sh
@@ -5,24 +5,28 @@ REPO_URL_BASE="https://dl.bintray.com/epam/reportportal"
 REPO_URL_JAR="$REPO_URL_BASE/com/epam/reportportal"
 
 # Versions of the services
-VERSION="5.0.0"
+API_VERSION="5.0.0"
+UAT_VERSION="5.0.0"
+ANALYZER_VERSION="5.0.0"
+MIGRATIONS_VERSION="5.0.0"
+UI_VERSION="5.0.0"
 SERVICE_INDEX_VERSION="5.0.5"
 
 # Downloading
 
-wget -c -N -O service-api.jar ${REPO_URL_JAR}/service-api/${VERSION}/service-api-${VERSION}-exec.jar
-wget -c -N -O service-uat.jar ${REPO_URL_JAR}/service-authorization/${VERSION}/service-authorization-${VERSION}-exec.jar
-wget -c -N -O service-analyzer ${REPO_URL_BASE}/${VERSION}/service-analyzer_linux_amd64
+wget -c -N -O service-api.jar ${REPO_URL_JAR}/service-api/${API_VERSION}/service-api-${API_VERSION}-exec.jar
+wget -c -N -O service-uat.jar ${REPO_URL_JAR}/service-authorization/${UAT_VERSION}/service-authorization-${UAT_VERSION}-exec.jar
+wget -c -N -O service-analyzer ${REPO_URL_BASE}/${ANALYZER_VERSION}/service-analyzer_linux_amd64
 wget -c -N -O service-index ${REPO_URL_BASE}/${SERVICE_INDEX_VERSION}/service-index_linux_amd64
-wget -c -N -O migrations.zip https://github.com/reportportal/migrations/archive/${VERSION}.zip && unzip migrations.zip && mv migrations-${VERSION} migrations && rm -f migrations.zip
+wget -c -N -O migrations.zip https://github.com/reportportal/migrations/archive/${MIGRATIONS_VERSION}.zip && unzip migrations.zip && mv migrations-${MIGRATIONS_VERSION} migrations && rm -f migrations.zip
 
 #UI
 #https://github.com/reportportal/service-ui/blob/master/Dockerfile
 
 mkdir ui
-wget -c -N -O service-ui ${REPO_URL_BASE}/${VERSION}/service-ui_linux_amd64 && mv service-ui ui
+wget -c -N -O service-ui ${REPO_URL_BASE}/${UI_VERSION}/service-ui_linux_amd64 && mv service-ui ui
 chmod -R +x ui/*
-wget -c -N -O ui.tar.gz ${REPO_URL_BASE}/${VERSION}/ui.tar.gz
+wget -c -N -O ui.tar.gz ${REPO_URL_BASE}/${UI_VERSION}/ui.tar.gz
 mkdir public
 tar -zxvf ui.tar.gz -C public && rm -f ui.tar.gz
 
